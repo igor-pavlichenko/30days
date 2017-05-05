8de3ede71834fe59e57325147b2b03d3d888c95f
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-05 18:13:54 +0100

    Add pre-built components and get ready to fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import 'whatwg-fetch';
+import './App.css';
+import TimeForm from './TimeForm';
 
-const a = [1, 10, 100, 1000, 10000];
-const App = (props) => {
-	return (
-		<ul>
-			{/*{a.map(i => {
-				return <li key={i}>{i}</li>
-			})}*/}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentTime: null, msg: 'now'
+		}
+	}
 
-			{React.Children.map(a, (i) => <li>{i}</li>)}
-		</ul>
-	)
+	// methods we'll fill in shortly
+	fetchCurrentTime() { }
+	getApiUrl() { }
+	handleFormSubmit(evt) { }
+	handleChange(newState) { }
+
+	render() {
+		const { currentTime, tz } = this.state;
+		const apiUrl = this.getApiUrl();
+
+		return (
+			<div>
+				{!currentTime &&
+					<button onClick={this.fetchCurrentTime.bind(this)}>
+						Get the current time
+					</button>}
+				{currentTime && <div>The current time is: {currentTime}</div>}
+				<TimeForm
+					onFormSubmit={this.handleFormSubmit.bind(this)}
+					onFormChange={this.handleChange.bind(this)}
+					tz={tz}
+					msg={'now'}
+				/>
+				<p>We'll be making a request from: <code>{apiUrl}</code></p>
+			</div>
+		)
+	}
 }
 
-export default App
+export default App;
