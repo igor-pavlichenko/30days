7ebc3bc8032369ba5118016cde0047b018334ca0
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-05 16:51:42 +0100

    Use React.Children.map

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const a = [1, 10, 100, 1000, 10000];
+const App = (props) => {
+	return (
+		<ul>
+			{/*{a.map(i => {
+				return <li key={i}>{i}</li>
+			})}*/}
+
+			{React.Children.map(a, (i) => <li>{i}</li>)}
+		</ul>
+	)
+}
+
+export default App
