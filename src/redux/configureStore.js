7503f822ab9d02404f35ebe46a874592be89b25c
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-10 10:47:55 +0100

    Add logging middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import { rootReducer, initialState } from './reducers';
+
+// MIDDLEWARE
+const loggingMiddleware = (store) => (next) => (action) => {
+	// Our middleware
+	console.log('Redux Log: ', action)
+	// call the next function
+	next(action);
+}
 
 
 export const configureStore = () => {
 	const store = createStore(
 		rootReducer,
 		initialState,
+		applyMiddleware(
+			loggingMiddleware,
+		),
 	);
 
 	return store;
