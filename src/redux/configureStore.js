a4d98091e0ff64266f32e643bde9d2dab0850b58
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-10 12:12:58 +0100

    Use redux devtools

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import { rootReducer, initialState } from './reducers/reducers';
 import loggingMiddleware from './middleware/loggingMiddleware';
@@ -6,12 +6,16 @@ import apiMiddleware from './middleware/apiMiddleware';
 
 
 export const configureStore = () => {
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 	const store = createStore(
 		rootReducer,
 		initialState,
-		applyMiddleware(
-			loggingMiddleware,
-			apiMiddleware,
+		composeEnhancers(
+			applyMiddleware(
+				loggingMiddleware,
+				apiMiddleware,
+			)
 		),
 	);
 
