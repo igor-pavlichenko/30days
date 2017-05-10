64d7cc547672d24e140a2eb600a5354fef90f440
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-10 10:53:05 +0100

    Insert a meta object in fetchNewTime action in order to let our api middleware catch it

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,9 +1,14 @@
 import * as types from './types';
 
+const host = 'https://andthetimeis.com'
 // seems like we are explicily exporting a function here
-export const fetchNewTime = () => ({
+export const fetchNewTime = ({timezone = 'pst', str = 'now'}) => ({
 	type: types.FETCH_NEW_TIME,
 	payload: new Date().toString(),
+	meta: {
+		type: 'api',
+		url: host + '/' + timezone + '/' + str + '.json'
+	},
 })
 
 export const login = (user) => ({
